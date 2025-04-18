0599b98d240346d99f2f95558e02df9c61fb7b11
qq5460168 <109363085+qq5460168@users.noreply.github.com>
2025-04-18 23:25:16 +0800

    删除 merge script.py

--- merge script.py
+++
@@ -1,6 +0,0 @@
-- name: Run merge script
-  run: |
-    python merge_rules.py
-    echo "规则更新时间: $(date -u '+%Y-%m-%d %H:%M:%S')" >> update.log
-    # 检查文件是否生成
-    ls -la merged-filter.txt update.log
