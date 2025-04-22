35b45c277ad91fdff80f512bbb469db797ecd6e5
qq5460168 <109363085+qq5460168@users.noreply.github.com>
2025-04-23 00:06:32 +0800

    Update AD.sh

--- shell/AD.sh
+++ shell/AD.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# 定义输出文件路径（与脚本同目录）
-OUTPUT_FILE="$(dirname "$0")/AD.txt"
+# 定义输出文件路径（主目录）
+OUTPUT_FILE="$HOME/AD.txt"
 
 # 下载文件并直接保存
 curl -s -o "${OUTPUT_FILE}" https://raw.githubusercontent.com/qq5460168/Who520/refs/heads/main/black.txt
