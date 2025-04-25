d8b87ecb57a04634b8987836602f0dc764625f3d
qq5460168 <109363085+qq5460168@users.noreply.github.com>
2025-04-25 15:11:18 +0800

    更新 AD.sh

--- AD.sh
+++ AD.sh
@@ -1,6 +1,6 @@
 !/bin/bash
 # 定义输出文件路径（与脚本同目录）
-OUTPUT_FILE="$(dirname "$0")/AD.txt"
+OUTPUT_FILE="$(dirname "$0")/ABD.txt"
 
 # 下载文件并直接保存
 curl -s -o "${OUTPUT_FILE}" https://raw.githubusercontent.com/qq5460168/666/master/dns.txt
