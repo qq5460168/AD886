69ed0ed0e78a80a0f0450db8fa3ccf61ae76dd74
qq5460168 <109363085+qq5460168@users.noreply.github.com>
2025-04-25 14:51:37 +0800

    更新 AD.sh

--- shell/AD.sh
+++ shell/AD.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 下载 dns.txt 文件到主目录并重命名为 AD.txt
-wget -O ~/AD.txt https://raw.githubusercontent.com/qq5460168/666/master/dns.txt
+# 使用 curl 下载 dns.txt 文件到主目录并重命名为 AD.txt
+curl -o ~/AD.txt https://raw.githubusercontent.com/qq5460168/666/master/dns.txt
 
 # 提示下载完成
 echo "文件已成功下载并保存为 ~/AD.txt"
