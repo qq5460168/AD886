614759acd769be0510ed4af998db36ca087120ef
qq5460168 <109363085+qq5460168@users.noreply.github.com>
2025-04-25 23:04:15 +0800

    Update AD.sh

--- shell/AD.sh
+++ shell/AD.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# 下载 dns.txt 文件到主目录并重命名为 AD.txt
-wget -O ~/AD.txt https://raw.githubusercontent.com/qq5460168/666/master/dns.txt
+# 定义变量，便于维护和复用
+URL="https://raw.githubusercontent.com/qq5460168/666/master/dns.txt"
+OUTPUT_FILE="$HOME/AD.txt"
 
-# 提示下载完成
-echo "文件已成功下载并保存为 ~/AD.txt"
+# 下载文件到主目录并重命名
+if wget -q -O "$OUTPUT_FILE" "$URL"; then
+    # 提示下载完成
+    echo "文件已成功下载并保存为 $OUTPUT_FILE"
+else
+    # 提示下载失败
+    echo "文件下载失败，请检查 URL 或网络连接"
+fi
