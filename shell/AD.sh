40fbfba397726a0c26f49c337e918c31a4ad0a4c
qq5460168 <109363085+qq5460168@users.noreply.github.com>
2025-09-02 19:44:46 +0800

    删除 AD.sh

--- shell/AD.sh
+++
@@ -1,8 +0,0 @@
-# AD.sh
-+#!/usr/bin/env bash
-+set -euo pipefail  # 严格错误处理
-
-# 增加证书验证
-+wget --ca-certificate=/etc/ssl/certs/ca-certificates.crt \
-+     -O "${TARGET_DIR}/AD.txt" \
-+     https://raw.githubusercontent.com/qq5460168/666/master/dns.txt
